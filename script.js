f668ddb2394716a24ca37a2780a181656d2d2b5a
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-10-13 12:25:12 +0200

    const

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-
-
+// Get references to the input text boxes //
+const tbxNum1 = document.getElementById("tbxNum1");
+const tbxNum2 = document.getElementById("tbxNum2");
+const tbxNum3 = document.getElementById("tbxNum3");
