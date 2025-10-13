6dbeece94c7e17b4699a8546a5d7e377726cb935
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-10-13 12:47:18 +0200

    Doesn´t work

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const tbxNum2 = document.getElementById("tbxNum2");
 const tbxNum3 = document.getElementById("tbxNum3");
 
 // Get references to the output text boxes //
-const txqareaResults = document.getElementById("txaResults");
+const txaResults = document.getElementById("txaResults");
 
-function findLargestNumber(params) {
-    let num1 = tbxNum1.value;
-    let num2 = tbxNum2.value;
-    let num3 = tbxNum3.value;
+function findLargestNumber() {
+    let tbxNum1 = tbxNum1.value;
+    let tbxNum2 = tbxNum2.value;
+    let tbxNum3 = tbxNum3.value;
+    txaResults.value = tbxNum1 + tbxNum2 + tbxNum3;
 
 
 }
