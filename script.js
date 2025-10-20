818135fd6c07f4961205e08bc9eede817177ff4a
rimet08 <rmetzner@proton.me>
2025-10-20 18:44:52 +0200

    Fixed

--- script.js
+++ script.js
@@ -3,32 +3,27 @@ const tbxNum1 = document.getElementById("tbxNum1");
 const tbxNum2 = document.getElementById("tbxNum2");
 const tbxNum3 = document.getElementById("tbxNum3");
 
-// Get references to the output text boxes //
-const txaResults = document.getElementById("txaResults");
+// Get reference to the output element //
+const resultDiv = document.getElementById("result");
 
 function findLargestNumber() {
     let Num1 = Number(tbxNum1.value);
     let Num2 = Number(tbxNum2.value);
     let Num3 = Number(tbxNum3.value);
     
-    if (Num1>Num2) {
-     //true//
-
+    let largest;
+    
+    if (Num1 > Num2) {
+        largest = Num1;
     }
     else {
         largest = Num2;
-        //false//
-
     }
 
-    if (largest<Num3) {
-
+    if (largest < Num3) {
+        largest = Num3;
     }
 
-    else {
-        largest = Num2;
-    }
-
-
-    txaResults.value = "The largest number is: " + largest;
+    resultDiv.textContent = "The largest number is: " + largest;
+    resultDiv.style.display = "block";
 }
